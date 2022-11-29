4647494c90e1670a7fbd01b7da8c34203a834128
ellewilliams <32607767+ellewilliams@users.noreply.github.com>
2022-11-29 16:07:01 +1100

    update iframe

--- src/components/digital/inline-iframe.tsx
+++ src/components/digital/inline-iframe.tsx
@@ -26,6 +26,7 @@ export const InlineIframe = ({ block, title }) => {
 						className="absolute top-0 left-0 right-0 bottom-0 h-full w-full"
 						src={block.hyperlink}
 						title={title}
+						allow="camera"
 					></iframe>
 				</div>
     </div>
